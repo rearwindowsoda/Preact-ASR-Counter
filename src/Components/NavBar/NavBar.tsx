b4114359a49f45380c54fe44cc24b4f001b79b88
MateuszOwczarek <mateuszowczarek@onet.eu>
2023-01-21 18:39:26 +0100

    navbar component

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+export default function NavBar() {
+	return (<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/">ASR Points</a>
+			<div id="navbarColor01">
+				<ul class="navbar-nav me-auto">
+					<li class="nav-item">
+						<a class="nav-link active" href="/">Home
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" href="/user">User panel
+						</a>
+					</li>
+				</ul>
+			</div></div><div></div></nav>
+	);
+}
