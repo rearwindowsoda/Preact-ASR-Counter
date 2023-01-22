93a94cee4675065c056760f507cffe7e22e54cb3
MateuszOwczarek <mateuszowczarek@onet.eu>
2023-01-22 01:23:57 +0100

    modified css

--- src/Components/Points/PointsCountStatus.tsx
+++ src/Components/Points/PointsCountStatus.tsx
@@ -5,7 +5,12 @@ import "./Points.css";
 export default function PointCountStatus () {
 	return(
 		<>
-			<div><h2 class={"main-counter"}>{count}</h2></div>
+			<div class="card text-white bg-success mb-3" style="max-width: 20rem;">
+				<div class="card-header">Your points:</div>
+				<div class="card-body">
+					<p class="card-text text-center">{count}</p>
+				</div>
+			</div>
 			<div><h5>Points multiplied:</h5>
 				<p class={"secondary-counter"}>{count.value * 30}</p></div>
 		</>
